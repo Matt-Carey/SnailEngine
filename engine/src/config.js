3ac2661afa025680d99bd1649343d33706dfdffe
Matt Carey <MattCarey@gmx.com>
2022-03-09 22:49:25 -0500

    Config.js to pull from current dir in browser

--- engine/src/config.js
+++ engine/src/config.js
@@ -1,13 +1,21 @@
 import { JSONFactory } from './factory/jsonFactory.js';
+import { IS_BROWSER } from './util/env.js';
 
 class Config {
     static #cfg = null;
 
     // Is called once by Engine, then modules requiring Config can call Config.get();
     static async load() {
-        return await JSONFactory.get('/config.json').then(json =>{
-            Config.#cfg = json;
-        });
+        if(IS_BROWSER) {
+            return await JSONFactory.get(document.URL.substring(0, document.URL.lastIndexOf('/')) + '/config.json').then(json =>{
+                Config.#cfg = json;
+            });
+        }
+        else {
+            return await JSONFactory.get('/config.json').then(json =>{
+                Config.#cfg = json;
+            });
+        }
     }
 
     static get() {
